db3cb06ddfc013fcaea31e99ae8f2b732fa33c9c
ibrahim salah <ibrahim.salah.officiel@gmail.com>
2025-12-12 14:51:59 +0100

    convert to json

--- docling_converter.py
+++ docling_converter.py
@@ -1,4 +1,5 @@
 from docling.document_converter import DocumentConverter
+import json
 
 def docx2markdown(file_path:str):
     converter = DocumentConverter()
@@ -7,6 +8,13 @@ def docx2markdown(file_path:str):
     output_path = "./markdown_docs/" + file_path.split("/")[-1].replace(".docx", ".md")
     with open(output_path, "w", encoding="utf-8") as f:
         f.write(result.document.export_to_markdown())
+def docx2json(file_path:str):
+    converter = DocumentConverter()
+    result = converter.convert(file_path)
+    # save result to a json file in mardown_docs folder
+    output_path = "./json_docs/" + file_path.split("/")[-1].replace(".docx", ".json")
+    with open(output_path, "w", encoding="utf-8") as f:
+        f.write(json.dumps(result.document.export_to_dict(), indent=2, ensure_ascii=False))
 
 
 # loop through all docx files in the docx_docs folder and convert them to markdown
@@ -15,4 +23,5 @@ docx_folder = "./docx_formation/"
 for file_name in os.listdir(docx_folder):
     if file_name.endswith(".docx"):
         file_path = os.path.join(docx_folder, file_name)    
-        docx2markdown(file_path=file_path)
+        docx2json(file_path=file_path)
+
